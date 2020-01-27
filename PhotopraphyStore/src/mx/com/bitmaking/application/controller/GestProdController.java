a3e315b39a2e799050b6d7537d1ada230a5cbf96
Albcervantes <albertce176@gmail.com>
2020-01-26 22:03:20 -0600

    termina gestion de catalogo de productos

--- PhotopraphyStore/src/mx/com/bitmaking/application/controller/GestProdController.java
+++ PhotopraphyStore/src/mx/com/bitmaking/application/controller/GestProdController.java
@@ -45,8 +45,10 @@ public class GestProdController {
 	private JFXButton btnEdtProd;
 	@FXML
 	private JFXButton btnSalir;
-	@FXML private JFXButton btnAcceptModif;
-	@FXML private JFXButton btnCancenModif;
+	@FXML
+	private JFXButton btnAcceptModif;
+	@FXML
+	private JFXButton btnCancenModif;
 	@FXML
 	private JFXTextField inputName;
 	@FXML
@@ -58,7 +60,8 @@ public class GestProdController {
 	private TreeView<String> treeProd;
 	private Stage stageProd = null;
 	LinkedHashMap<Integer, Store_cat_prod> productsMap = null;
-	
+	Store_cat_prod catProdModif = null;
+
 	public JFXButton getBtnSalir() {
 		return btnSalir;
 	}
@@ -69,16 +72,25 @@ public class GestProdController {
 		btnAddProd.addEventHandler(MouseEvent.MOUSE_CLICKED, editProd("A"));
 		btnEdtProd.addEventHandler(MouseEvent.MOUSE_CLICKED, editProd("M"));
 		treeProd.addEventHandler(MouseEvent.MOUSE_CLICKED, showDetails());
+		btnAcceptModif.addEventHandler(MouseEvent.MOUSE_CLICKED, acceptModifProd());
 		btnEliminarProd.addEventHandler(MouseEvent.MOUSE_CLICKED, modalDelProd());
 
 	}
 
-	private EventHandler<MouseEvent> acceptDelProd() {
+	private EventHandler<MouseEvent> acceptDelProd(TreeItem<String> treeItem) {
 		return new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
-				
+
 				try {
+					deleteChildren(treeItem);
+					String[] arrayStr = treeItem.getValue().split("\\|");
+
+					String idProd = arrayStr[0].substring(2, arrayStr[0].length()).trim();
+					Store_cat_prod row = productsMap.get(Integer.parseInt(idProd));
+					row.setEstatus(("ACTIVO".equals(row.getEstatus().toUpperCase())) ? "1" : "0");
+					catProdService.deleteRow(row);
+					
 					getTblCatProducts();
 					stageProd.close();
 
@@ -100,12 +112,18 @@ public class GestProdController {
 			public void handle(MouseEvent event) {
 				// System.out.println(event.getSource());
 				try {
-					ObservableList<Store_cat_prod> row = null;// tblProducts.getSelectionModel().getSelectedItems();
-					if (row == null || row.size() == 0) {
-						GeneralMethods.modalMsg("WARNING", "",
-								"Para eliminar un producto, debes seleccionar un registro");
+					if (productsMap == null || productsMap.size() == 0) {
+						GeneralMethods.modalMsg("WARNING", "", "No hay registro a eliminar");
 						return;
 					}
+
+					ObservableList<TreeItem<String>> objTree = treeProd.getSelectionModel().getSelectedItems();
+					TreeItem<String> treeItem = objTree.get(0);
+					if (treeItem == null) {
+						GeneralMethods.modalMsg("WARNING", "", "No hay registro seleccionado");
+						return;
+					}
+					int idx = treeProd.getSelectionModel().getSelectedIndex();
 
 					FXMLLoader fxmlLoader = new FXMLLoader(
 							getClass().getResource("/mx/com/bitmaking/application/view/ModalConfirm.fxml"));
@@ -123,14 +141,14 @@ public class GestProdController {
 					stageProd.setMaxWidth(550.0);
 					stageProd.initModality(Modality.APPLICATION_MODAL);
 
-					ModalConfirmController modalObj = fxmlLoader.getController(); // Obtiene controller de la nueva ventana
+					ModalConfirmController modalObj = fxmlLoader.getController(); 
 					List<String> lstStts = new ArrayList<>();
 					lstStts.add("Activo");
 					lstStts.add("Inactivo");
 					modalObj.getLblMsg().setText("¿Seguro que desea eliminar el producto?");
 
 					modalObj.getBtnCancelar().addEventHandler(MouseEvent.MOUSE_CLICKED, closeModalEditProd());
-					modalObj.getBtnConfirm().addEventHandler(MouseEvent.MOUSE_CLICKED, acceptDelProd());
+					modalObj.getBtnConfirm().addEventHandler(MouseEvent.MOUSE_CLICKED, acceptDelProd(treeItem));
 					stageProd.show();
 				} catch (Exception ex) {
 					ex.printStackTrace();
@@ -148,13 +166,14 @@ public class GestProdController {
 		lstStts.add("Inactivo");
 		cbxStts.getItems().removeAll(cbxStts.getItems());
 		cbxStts.setItems(FXCollections.observableList(lstStts));
-		
+
 		productsMap = catProdService.getAllCatalogoProduct2();
 		TreeItem<String> root = new TreeItem<>("Productos del cliente");
 		root.setExpanded(true);
 
 		generateTreeProd(productsMap, 0, root);
 		treeProd.setRoot(root);
+		treeProd.getSelectionModel().select(0);
 	}
 
 	//
@@ -185,37 +204,41 @@ public class GestProdController {
 		return new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
+				catProdModif = null;
 				inputName.setText("");
 				cbxStts.setValue("");
 				try {
 
 					if (productsMap == null || productsMap.size() == 0) {
-					//	GeneralMethods.modalMsg("WARNING", "", "No hay registro a modificar");
+						// GeneralMethods.modalMsg("WARNING", "", "No hay
+						// registro a modificar");
 						return;
 					}
 
 					ObservableList<TreeItem<String>> objTree = treeProd.getSelectionModel().getSelectedItems();
 					TreeItem<String> treeItem = objTree.get(0);
 					if (treeItem == null) {
-					//	GeneralMethods.modalMsg("WARNING", "", "No hay registro seleccionado");
+						// GeneralMethods.modalMsg("WARNING", "", "No hay
+						// registro seleccionado");
 						return;
 					}
 					int idx = treeProd.getSelectionModel().getSelectedIndex();
 
 					String strRow = treeItem.getValue();
 
-					if (strRow == null || strRow.length() == 0 || idx <= 0) {// ||  row.length()==0
-						//GeneralMethods.modalMsg("WARNING", "", "Para modificar, debes seleccionar un registro");
+					if (strRow == null || strRow.length() == 0 || idx <= 0) {// ||
+																				// row.length()==0
+						// GeneralMethods.modalMsg("WARNING", "", "Para
+						// modificar, debes seleccionar un registro");
 						return;
 					} else {
 						System.out.println(strRow);
 						String[] arrayStr = strRow.split("\\|");
 						System.out.println(arrayStr[0]);
 						String idProd = arrayStr[0].substring(2, arrayStr[0].length()).trim();
-						Store_cat_prod catProd = productsMap.get(Integer.parseInt(idProd));
-						inputName.setText(catProd.getProducto());
-						System.out.println("stts:"+catProd.getEstatus());
-						cbxStts.setValue(catProd.getEstatus());
+						catProdModif = productsMap.get(Integer.parseInt(idProd));
+						inputName.setText(catProdModif.getProducto());
+						cbxStts.setValue(catProdModif.getEstatus());
 					}
 				} catch (Exception ex) {
 					ex.printStackTrace();
@@ -224,6 +247,95 @@ public class GestProdController {
 		};
 	}
 
+	private EventHandler<MouseEvent> acceptModifProd() {
+		return new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+
+				try {
+
+					if (productsMap == null || productsMap.size() == 0) {
+						// GeneralMethods.modalMsg("WARNING", "", "No hay
+						// registro a modificar");
+						return;
+					}
+
+					ObservableList<TreeItem<String>> objTree = treeProd.getSelectionModel().getSelectedItems();
+					TreeItem<String> treeItem = objTree.get(0);
+					if (treeItem == null) {
+						// GeneralMethods.modalMsg("WARNING", "", "No hay
+						// registro seleccionado");
+						return;
+					}
+					int idx = treeProd.getSelectionModel().getSelectedIndex();
+
+					String strRow = treeItem.getValue();
+
+					if (strRow == null || strRow.length() == 0 || idx <= 0) {// ||
+																				// row.length()==0
+						// GeneralMethods.modalMsg("WARNING", "", "Para
+						// modificar, debes seleccionar un registro");
+						return;
+					} else {
+						cancelModif();
+						Store_cat_prod row = catProdModif;
+						row.setProducto(inputName.getText());
+						row.setEstatus(("ACTIVO".equals(cbxStts.getValue().toUpperCase())) ? "1" : "0");
+						catProdService.insertRow(row);
+						// if("0".equals(row.getEstatus())){
+						inactiveChildren(treeItem, row.getEstatus());
+						// }
+						cancelModif();
+						inputName.setText("");
+						cbxStts.setValue("");
+						getTblCatProducts();
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+
+		};
+	}
+
+	private void inactiveChildren(TreeItem<String> treeItem, String stts) {
+		ObservableList<TreeItem<String>> children = treeItem.getChildren();
+		// TreeItem<String> item = null;
+		String[] arrayStr = null;
+		String idProd = null;
+		Store_cat_prod row = null;
+		for (TreeItem<String> el : children) {
+
+			arrayStr = el.getValue().split("\\|");
+
+			idProd = arrayStr[0].substring(2, arrayStr[0].length()).trim();
+			row = productsMap.get(Integer.parseInt(idProd));
+			row.setEstatus(stts);
+			catProdService.insertRow(row);
+			inactiveChildren(el, stts);
+
+		}
+
+	}
+
+	private void deleteChildren(TreeItem<String> treeItem) {
+		ObservableList<TreeItem<String>> children = treeItem.getChildren();
+		// TreeItem<String> item = null;
+		String[] arrayStr = null;
+		String idProd = null;
+		Store_cat_prod row = null;
+		for (TreeItem<String> el : children) {
+
+			arrayStr = el.getValue().split("\\|");
+
+			idProd = arrayStr[0].substring(2, arrayStr[0].length()).trim();
+			row = productsMap.get(Integer.parseInt(idProd));
+			catProdService.deleteRow(row);
+			deleteChildren(el);
+
+		}
+
+	}
 	/**
 	 * 
 	 * @param typeForm:
@@ -257,21 +369,26 @@ public class GestProdController {
 						stageProd.setMaxWidth(350.0);
 						stageProd.initModality(Modality.APPLICATION_MODAL);
 
-						EditaProdController edtProd = fxmlLoader.getController(); 
+						EditaProdController edtProd = fxmlLoader.getController();
 						List<String> lstStts = new ArrayList<>();
 						lstStts.add("Activo");
 						lstStts.add("Inactivo");
 						edtProd.getCbxEstatusProd().getItems().removeAll(edtProd.getCbxEstatusProd().getItems());
 						edtProd.getCbxEstatusProd().setItems(FXCollections.observableList(lstStts));
 						edtProd.getCbxEstatusProd().setValue("Activo");
+						edtProd.getBtnAccept().addEventHandler(MouseEvent.MOUSE_CLICKED,
+								acceptEditProd(edtProd, typeForm));
+						edtProd.getBtnCancel().addEventHandler(MouseEvent.MOUSE_CLICKED, closeModalEditProd());
+
 						stageProd.show();
 						break;
 					case "M":
+						treeProd.setDisable(true);
 						btnAcceptModif.setVisible(true);
 						btnCancenModif.setVisible(true);
 						cbxStts.setDisable(false);
 						inputName.setDisable(false);
-						
+
 						break;
 					default:
 						GeneralMethods.modalMsg("Error", "", "No fue posible identificar la operación");
@@ -284,14 +401,15 @@ public class GestProdController {
 			}
 		};
 	}
-	
+
 	@FXML
-	private void cancelModif(){
+	private void cancelModif() {
 		btnAcceptModif.setVisible(false);
 		btnCancenModif.setVisible(false);
 		cbxStts.setDisable(true);
 		inputName.setDisable(true);
-		
+		treeProd.setDisable(false);
+
 	}
 
 	private EventHandler<MouseEvent> acceptEditProd(EditaProdController edtProd, String typeForm) {
@@ -304,6 +422,11 @@ public class GestProdController {
 
 				row.setProducto(edtProd.getInputProdName().getText());
 				row.setEstatus(("ACTIVO".equals(edtProd.getCbxEstatusProd().getValue().toUpperCase())) ? "1" : "0");
+				if (catProdModif == null) {
+					row.setId_padre_prod(0);
+				} else {
+					row.setId_padre_prod(catProdModif.getId_prod());
+				}
 				try {
 					if ("M".equals(typeForm)) {
 						ObservableList<Store_cat_prod> lsRow = null;// tblProducts.getSelectionModel().getSelectedItems();
