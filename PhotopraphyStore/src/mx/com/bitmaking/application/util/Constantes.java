64ae8f0eace68ae4c0ba5e62c4b6bc3358dbd803
Alberto A <albertce176@gmail.com>
2020-03-01 20:25:26 -0600

    tamaño de caracteres en ticket

--- PhotopraphyStore/src/mx/com/bitmaking/application/util/Constantes.java
+++ PhotopraphyStore/src/mx/com/bitmaking/application/util/Constantes.java
@@ -7,6 +7,6 @@ public class Constantes {
 	public static final String PATH_XLS = /*"C://Reportes/";*/  "/Users/Gar/Documents/";//"C://Reportes/";
 	public static final int MAX_INTENTOS=3;
 	public static final int NUMBER_LENGTH = 10;
-	public static final int MAX_CHARS_TICKET = 30;
+	public static final int MAX_CHARS_TICKET = 40;
 	public static final String PRINTER_NAME = "EPSON-TM1";
 }
